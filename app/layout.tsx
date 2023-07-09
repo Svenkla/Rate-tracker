08bf209b1b9467e8fa1b06b79fba5a0b840b83d7
Svenkla <sven.plibersek@gmail.com>
2023-07-09 23:42:11 +0200

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,8 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: any;
 }) {
   return (
     <html lang="en">
