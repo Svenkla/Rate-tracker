680dc4d56219e91c43bdd7730229327be00a3145
Svenkla <sven.plibersek@gmail.com>
2023-07-09 23:40:37 +0200

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <SessionProvider session={session}>
+      <SessionProvider>
         <body>{children}</body>
       </SessionProvider>
     </html>
